f7c6584c47e65b3df46955c2577307ab30bc2dad
Md. Pappu Miahn <cse.pappu@gmail.com>
2022-03-06 22:43:11 +0600

    Css update

--- client/src/components/UrlSorten.js
+++ client/src/components/UrlSorten.js
@@ -1,96 +1,130 @@
-import { React, useState, useEffect, useRef } from 'react';
-import axios from 'axios';
-import validator from 'validator';
-import copy from 'copy-text-to-clipboard';
+import { React, useState, useEffect, useRef } from "react";
+import axios from "axios";
+import validator from "validator";
+import copy from "copy-text-to-clipboard";
 
 const UrlSorten = () => {
-    const inputRef = useRef(null);
-    const [shortenURLs, setShortenURLs] = useState([]);
-    const [inputText, setInput] = useState(''); // '' is the initial state value
-    const [errMsg, setErrMsg] = useState('');
-    const [isCopy, setCopy] = useState('');
-    const baseUrl = 'http://localhost:3000/';
+  const inputRef = useRef(null);
+  const [shortenURLs, setShortenURLs] = useState([]);
+  const [inputText, setInput] = useState(""); // '' is the initial state value
+  const [errMsg, setErrMsg] = useState("");
+  const [isCopy, setCopy] = useState("");
+  const baseUrl = "http://localhost:3000/";
 
-    useEffect(() => {
-        // Get all shorted link list
-        axios.get('http://localhost:4000/allShortenUrls').then(res => {
-            const { shortenUrls } = res.data || [];
-            setShortenURLs(shortenUrls);
+  useEffect(() => {
+    // Get all shorted link list
+    axios.get("http://localhost:4000/allShortenUrls").then((res) => {
+      const { shortenUrls } = res.data || [];
+      setShortenURLs(shortenUrls);
+    });
+  }, [shortenURLs]);
+
+  const handelShortenUrl = (e) => {
+    let url = inputText;
+    let isValidDomain = validator.isURL(url, { require_valid_protocol: false }); // Domain validation check
+
+    if (!url || !isValidDomain) {
+      setErrMsg("Please, provide a valid url");
+      destroyNotification(true, setErrMsg);
+    } else {
+      axios
+        .post("http://localhost:4000/generateShortenUrl", { url })
+        .then((res) => {
+          const { slug } = res.data;
+          if (slug) {
+            setInput(baseUrl + slug);
+            setCopy(true);
+          }
         });
-    }, [shortenURLs]);
+    }
+  };
 
-    const handelShortenUrl = (e) => {
-        let url = inputText;
-        let isValidDomain = validator.isURL(url, { require_valid_protocol: false }); // Domain validation check
+  // Copied input text
+  const handelCopyShortenUrl = (e) => {
+    inputRef.current.select();
+    copy(inputText);
+  };
 
-        if (!url || !isValidDomain) {
-            setErrMsg("Please, provide a valid url");
-            destroyNotification(true, setErrMsg);
-        } else {
-            axios.post('http://localhost:4000/generateShortenUrl', { url }).then(res => {
-                const { slug } = res.data;
-                if (slug) {
-                    setInput(baseUrl + slug);
-                    setCopy(true);
-                }
-            });
-        }
-    };
+  // Copied short link url
+  const handelCopyShortenLink = (id) => {
+    let text = document.getElementById("616e87fa4aa6f85cbaba63f5").innerText;
+    copy(text);
+  };
 
-    // Copied input text
-    const handelCopyShortenUrl = (e) => {
-        inputRef.current.select();
-        copy(inputText);
-    };
+  // Destroy error notification
+  const destroyNotification = (isError, setErrMsg) => {
+    if (isError) {
+      setTimeout(function () {
+        setErrMsg("");
+      }, 2000);
+    }
+  };
 
-    // Copied short link url 
-    const handelCopyShortenLink = (id) => {
-        let text = document.getElementById('616e87fa4aa6f85cbaba63f5').innerText;
-        copy(text);
-    };
-
-    // Destroy error notification
-    const destroyNotification = (isError, setErrMsg) => {
-        if (isError) {
-            setTimeout(function () {
-                setErrMsg('');
-            }, 2000);
-        }
-    };
-
-    return (
-        <div className="container">
-            <div><h1 className="page-title">Generate Short Link</h1></div>
-            <div className="form-row align-items-center">
-                <div className="col-auto">
-                    <input type="text" ref={inputRef} className="form-control sorten-link" placeholder="Shorten your link" onInput={e => setInput(e.target.value)} value={inputText} />
-                    {isCopy ? (
-                        <button className="btn btn-primary shorten-btn" onClick={handelCopyShortenUrl}>Copy</button>
-                    ) : (
-                        <button className="btn btn-primary shorten-btn" onClick={handelShortenUrl}>Shorten</button>
-                    )}
+  return (
+    <div className="container">
+      <div>
+        <h1 className="page-title">Generate Short Link</h1>
+      </div>
+      <div className="form-row align-items-center">
+        <div className="col-auto">
+          <input
+            type="text"
+            ref={inputRef}
+            className="form-control sorten-link"
+            placeholder="Shorten your link"
+            onInput={(e) => setInput(e.target.value)}
+            value={inputText}
+          />
+          {isCopy ? (
+            <button
+              className="btn btn-primary shorten-btn"
+              onClick={handelCopyShortenUrl}
+            >
+              Copy
+            </button>
+          ) : (
+            <button
+              className="btn btn-primary shorten-btn"
+              onClick={handelShortenUrl}
+            >
+              Shorten
+            </button>
+          )}
+        </div>
+        {errMsg && (
+          <div className="alert alert-danger mt-20" role="alert">
+            {errMsg}
+          </div>
+        )}
+        <div className="card mt-20">
+          <div className="card-body">
+            {shortenURLs.map((shortenURL, index) => (
+              <div className="row url-row" key={index}>
+                <div className="col-sm-6">{shortenURL.url}</div>
+                <div className="col-sm-6 text-right">
+                  <a
+                    className="mr-20"
+                    href={baseUrl + shortenURL.slug}
+                    id={shortenURL._id}
+                    target="_blank"
+                  >
+                    {baseUrl}
+                    {shortenURL.slug}
+                  </a>
+                  <button
+                    className="btn btn-primary"
+                    onClick={() => handelCopyShortenLink(shortenURL._id)}
+                  >
+                    Copy
+                  </button>
                 </div>
-                {errMsg &&
-                    <div className="alert alert-danger mt-20" role="alert">
-                        {errMsg}
-                    </div>
-                }
-                <div className="card mt-20">
-                    <div className="card-body">
-                        {shortenURLs.map((shortenURL, index) => (
-                            <div className="row url-row" key={index}>
-                                <div className="col-sm-6">{shortenURL.url}</div>
-                                <div className="col-sm-6 text-right">
-                                    <a className="mr-20" href={baseUrl + shortenURL.slug} id={shortenURL._id} target="_blank">{baseUrl}{shortenURL.slug}</a>
-                                    <button className="btn btn-primary" onClick={() => handelCopyShortenLink(shortenURL._id)}>Copy</button>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </div>
-        </div >
-    )
-}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default UrlSorten;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import path from 'path';
-import ShortenUrl from './models/shorten.model.js';
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import path from "path";
+import ShortenUrl from "./models/shorten.model.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -13,25 +13,25 @@ app.use(express.urlencoded());
 app.use(cors());
 
 let __dirname = path.resolve();
-dotenv.config({ path: __dirname + '/.env' });
+dotenv.config({ path: __dirname + "/.env" });
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
 
-connection.once('open', () => {
-  console.log('Mongo DB database connection established successfully!');
+connection.once("open", () => {
+  console.log("Mongo DB database connection established successfully!");
 });
 
 // Routes
 // Generate short url link
-app.post('/generateShortenUrl', (req, res) => {
+app.post("/generateShortenUrl", (req, res) => {
   const { url } = req.body;
   let randomId = makeid(6);
   let slug = randomId;
   const shortenData = new ShortenUrl({
     url,
-    slug
+    slug,
   });
 
   shortenData.save((error) => {
@@ -41,27 +41,29 @@ app.post('/generateShortenUrl', (req, res) => {
       res.send({ slug: slug, message: "successfully added!" });
     }
   });
-
 });
 
 // Redirect origin url from short link
-app.get('/redirectOriginUrl/:slug', (req, res) => {
+app.get("/redirectOriginUrl/:slug", (req, res) => {
   const { slug } = req.params;
-  const existsUrlInfo = ShortenUrl.findOne({ slug: slug }, function (err, shorten) {
-    if (shorten) {
-      res.send({
-        url: shorten.url,
-        shortenUrl: process.env.URL + slug,
-        message: "successfully added!"
-      });
-    } else {
-      res.send(error);
+  const existsUrlInfo = ShortenUrl.findOne(
+    { slug: slug },
+    function (err, shorten) {
+      if (shorten) {
+        res.send({
+          url: shorten.url,
+          shortenUrl: process.env.URL + slug,
+          message: "successfully added!",
+        });
+      } else {
+        res.send(error);
+      }
     }
-  });
+  );
 });
 
 // Get all short link Urls
-app.get('/allShortenUrls', (req, res) => {
+app.get("/allShortenUrls", (req, res) => {
   const shortenUrls = ShortenUrl.find({}, function (err, docs) {
     if (err) {
       res.send(err);
@@ -72,17 +74,16 @@ app.get('/allShortenUrls', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`)
+  console.log(`Server is running on port: ${port}`);
 });
 
-
 const makeid = (length) => {
-  let result = '';
-  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = "";
+  let characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   let charactersLength = characters.length;
   for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() *
-      charactersLength));
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
-}
+};
